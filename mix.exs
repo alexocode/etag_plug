85013076b5a7832c724f21f455a0869d23f9cc41
Sascha Wolf <sascha.wolf@grandcentrix.net>
2018-10-26 13:43:56 +0200

    Deps: Depend on Plug ~> 1.0

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule EtagPlug.MixProject do
+defmodule ETagPlug.MixProject do
   use Mix.Project
 
   def project do
@@ -21,8 +21,7 @@ defmodule EtagPlug.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:plug, "~> 1.0"}
     ]
   end
 end
