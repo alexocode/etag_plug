f0f2407744923062b72d479542b7392e1d2785af
Sascha Wolf <sascha.wolf@grandcentrix.net>
2018-10-26 17:04:08 +0200

    Project: Add the links and stuff for ExDoc and Hex.PM

--- mix.exs
+++ mix.exs
@@ -4,10 +4,19 @@ defmodule ETagPlug.MixProject do
   def project do
     [
       app: :etag_plug,
-      version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "ETag Plug",
+      source_url: "https://github.com/Zeeker/etag_plug",
+      homepage_url: "https://github.com/Zeeker/etag_plug",
+
+      # Hex
+      description: description(),
+      package: package(),
+      version: "0.1.0"
     ]
   end
 
@@ -22,6 +31,20 @@ defmodule ETagPlug.MixProject do
   defp deps do
     [
       {:plug, "~> 1.0"}
+    ]
+  end
+
+  def description do
+    "A simple to use plug for shallow ETags"
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Zeeker/etag_plug"
+      },
+      maintainers: ["Sascha Wolf <swolf.dev@gmail.com>"]
     ]
   end
 end
